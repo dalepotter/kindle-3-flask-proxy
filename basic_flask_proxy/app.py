e21b41c1e4b9fd72b2d1284ba5995631ddee6b39
dalepotter <dalepotter@gmail.com>
2021-12-27 23:48:25 +0000

    Send URLs referenced in HTML response back through the proxy

--- basic_flask_proxy/app.py
+++ basic_flask_proxy/app.py
@@ -1,7 +1,26 @@
+import re
 import requests
+from urllib.parse import urlparse
 from flask import request, Flask, Response
 
 app = Flask(__name__.split('.')[0])
+
+
+def convert_links(html, domain_called, proxy_prefix):
+    # Handle absolute imports
+    output = re.sub(
+        r'([href|src]=)["\'](.*)//(.*)["\']',
+        rf'\1"{proxy_prefix}\3"',
+        html
+    )
+
+    # Handle relative imports
+    output = re.sub(
+        r'([href|src]=)["\']/(.*)["\']',
+        rf'\1"{proxy_prefix}{domain_called}/\2"',
+        output
+    )
+    return output
 
 
 @app.route('/p/<path>', methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
@@ -22,5 +41,11 @@ def _proxy(path, *args, **kwargs):
     headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers]
 
-    response = Response(resp.content, resp.status_code, headers)
+    html = convert_links(
+        resp.content.decode("utf-8"),
+        domain_called=urlparse(url).netloc,
+        proxy_prefix=f"http://{request.host}/p/",
+    )
+
+    response = Response(html, resp.status_code, headers)
     return response
