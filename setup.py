ca80761974a986a3a57c00e6ab7a27ca0970979b
dalepotter <dalepotter@gmail.com>
2021-12-27 22:23:13 +0000

    Add python packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basic_flask_proxy',
+    version='0.0.1',
+    url='https://github.com/dalepotter/kindle-3-flask-proxy',
+    author='Dale Potter',
+    author_email='dalepotter@gmail.com',
+    description='Basic Flask proxy to support accessing modern websites using the Amazon Kindle 3 built-in web browser.',
+    packages=find_packages(),
+    install_requires=[
+        "Flask==2.0.2",
+        "requests==2.26.0",
+    ]
+)
