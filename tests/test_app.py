c5d143a459bb467c24eaa025b9b12c86c8a66270
dalepotter <dalepotter@gmail.com>
2022-01-02 22:24:32 +0000

    Add full support for relative URL references in HTML content

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,12 +10,14 @@ from basic_flask_proxy.app import app, convert_links
     ("https://www.example.com/path/to/file.jpg", "http://www.proxy.com/p?url=www.example.com/path/to/file.jpg"),
     ("//www.example.com/path/to/file.jpg", "http://www.proxy.com/p?url=www.example.com/path/to/file.jpg"),
     ("/path/to/file.jpg", "http://www.proxy.com/p?url=www.example.com/path/to/file.jpg"),
-    pytest.param("../abc.py", "http://www.proxy.com/p?url=www.example.com/abc.py", marks=pytest.mark.xfail),  # Relative links not yet supported
+    ("../abc.py", "http://www.proxy.com/p?url=www.example.com/path/abc.py"),
+    ("./abc.py", "http://www.proxy.com/p?url=www.example.com/path/to/abc.py"),
 ])
 def test_convert_links(html_element_attribute, quote_char, input_url, expected_output):
+    current_url = "www.example.com/path/to/file.txt"
     input_string = f'{html_element_attribute}={quote_char}{input_url}{quote_char}'
 
-    result = convert_links(input_string, domain_called="www.example.com", proxy_prefix="http://www.proxy.com/p?url=")
+    result = convert_links(input_string, current_url, proxy_prefix="http://www.proxy.com/p?url=")
 
     assert result == f'{html_element_attribute}="{expected_output}"'
 
